905345fdf7a46e66ad27b1e2d32c5b57b9af7a39
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-05 18:56:53 -0300

    feat: add next-seo config

---
+++ config/seo.ts
@@ -0,0 +1,12 @@
+const seoConfig = {
+  title: "Movie Directory CMS",
+  description: "Movie Directory made with Next.js and CMS",
+  openGraph: {
+    url: "https://moviedirectory.vercel.app/",
+    title: "Movie Directory made with CMS",
+    description: "Movie Directory made with Next.js and CMS, Open-source project.",
+    site_name: "Movie Directory CMS",
+  },
+};
+
+export default seoConfig;
