8c132a791e363933ff5dfe4dcd5468eac64d89e2
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-06 07:38:04 -0300

    feat: add theme container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { DefaultSeo } from 'next-seo'
+import ThemeContainer from '../contexts/theme/theme-container'
 
 import seoConfig from '../config/seo'
 
@@ -11,10 +12,10 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <>
+      <ThemeContainer>
         <DefaultSeo {...seoConfig} />
         <Component {...pageProps} />
-      </>
+      </ThemeContainer>
     </>
   )
 }
