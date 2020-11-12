15fe967ea35d14f266781b2f4d59d45c5d31b75f
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-12 08:25:16 -0300

    fix: add footer and header to _app.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import { Heading } from '@chakra-ui/core'
-import Header from '../components/header'
-import Footer from '../components/footer'
 
 export default function Home() {
   return (
     <>
-      <Header />
-
       <Heading>Hello World</Heading>
-
-      <Footer />
     </>
   )
 }
