e6fd27a1777d47a6198cc9e5fde94554e5e300ce
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-15 15:27:10 -0300

    feat: improve responsive in header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -53,7 +53,7 @@ const Header: React.FC = props => {
       justify="space-between"
       wrap="wrap"
       paddingX={{ base: 10, sm: 10, lg: 20 }}
-      paddingY="10"
+      paddingY={{ sm: '8', lg: '10' }}
       backgroundColor="white"
       zIndex={999}
       {...props}
@@ -72,14 +72,13 @@ const Header: React.FC = props => {
         width={{ sm: 'full', lg: 'auto' }}
         marginTop={{ sm: '20px', lg: '0' }}
       >
-        <Divider mt="8" />
         {navLinks.map(item => (
           <MenuItems key={item.label} onClick={handleToggle} link={item.link}>
             {item.label}
           </MenuItems>
         ))}
 
-        <Flex>
+        <Flex mt={{ sm: '10', lg: '0' }}>
           {socialLinks.map(item => (
             <ChakraLink href="https://chakra-ui.com" isExternal>
               <Icon as={item.icon} w={6} h={6} mr="8" color="gray.500" />
