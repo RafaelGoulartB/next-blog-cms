76644d42916166c88495722fbd5216bb0d6d95d4
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-15 15:43:45 -0300

    feat: add footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Heading } from '@chakra-ui/react'
+import { Heading, Flex } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <>
+    <Flex bg="gray.100">
       <Heading py="40">Hello World</Heading>
-    </>
+    </Flex>
   )
 }
--- styles/theme/styles.ts
+++ styles/theme/styles.ts
@@ -1,7 +1,7 @@
 const styles = {
   global: {
     body: {
-      bg: 'gray.100',
+      bg: 'white',
       color: 'black'
     }
   }
