63855f1bed377b6cdcacc1220d27469fb437af40
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-16 09:48:40 -0300

    feat: update post listing container

--- components/list-post/index.tsx
+++ components/list-post/index.tsx
@@ -3,7 +3,13 @@ import { Flex } from '@chakra-ui/react'
 
 const ListPost: React.FC = ({ children }) => {
   return (
-    <Flex maxW="1200px" w="100%" mx="auto" paddingX="10" boxSizing="border-box">
+    <Flex
+      maxW="1440px"
+      w="100%"
+      mx="auto"
+      paddingX={{ sm: '10', lg: '24' }}
+      boxSizing="border-box"
+    >
       {children}
     </Flex>
   )
