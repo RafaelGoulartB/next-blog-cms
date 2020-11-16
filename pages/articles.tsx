fcac72c1f671c95a0ffa8eed18c34247cc3c1986
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-16 13:29:28 -0300

    feat: add articles page

--- pages/articles.tsx
+++ pages/articles.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Box } from '@chakra-ui/react'
+import ListPost from '../components/list-post'
+import LatestCard from '../components/latest-card'
+import { posts } from '../data/constants'
 
 // import { Container } from './styles';
 
 const About: React.FC = () => {
-  return <div />
+  return (
+    <Box as="main">
+      <ListPost>
+        {posts.map(post => (
+          <LatestCard
+            key={post.id}
+            id={post.id}
+            title={post.title}
+            description={post.description}
+            image={post.image}
+            date={post.date}
+          />
+        ))}
+      </ListPost>
+    </Box>
+  )
 }
 
 export default About
