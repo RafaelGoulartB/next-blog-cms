322ee0245b5de27b1f38527e65c4ecd3f7c8afd0
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-18 08:29:05 -0300

    style: add flexWrap in recentPost container

--- components/post-card/container.tsx
+++ components/post-card/container.tsx
@@ -3,7 +3,12 @@ import { Flex } from '@chakra-ui/react'
 
 const PostCardContainer: React.FC = ({ children }) => {
   return (
-    <Flex mt="12" flexDir={{ sm: 'column', lg: 'row' }} justify="space-between">
+    <Flex
+      mt="12"
+      flexDir={{ sm: 'column', lg: 'row' }}
+      justify="space-between"
+      flexWrap="wrap"
+    >
       {children}
     </Flex>
   )
