2bb80d26538469ad9b8689130f531c90ae2947e7
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-22 18:06:41 -0300

    feat: add contants and types for post page

---
+++ pages/posts/constants.ts
@@ -0,0 +1,64 @@
+import { PostProps } from "./types"
+
+export const PostFixtures: PostProps[] = [
+  {
+    id: '0',
+    title: 'long established',
+    description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that....',
+    text: 'Test Test',
+    image: '/images/posts/latest-post.png',
+    date: new Date(),
+    highlight: false
+  },
+  {
+    id: '1',
+    title: 'long established',
+    description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that....',
+    text: 'Test Test',
+    image: '/images/posts/post-1.png',
+    date: new Date(),
+    highlight: false
+  },
+  {
+    id: '2',
+    title: 'long established',
+    description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that....',
+    text: 'Test Test',
+    image: '/images/posts/post-2.png',
+    date: new Date(),
+    highlight: false
+  },
+  {
+    id: '3',
+    title: 'long established',
+    description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that....',
+    image: '/images/posts/post-3.png',
+    text: 'Test Test',
+    date: new Date(),
+    highlight: false
+  },
+  {
+    id: '4',
+    title: 'What is Lorem Ipsum?',
+    description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution...',
+    text: 'Test Test',
+    image: '/images/posts/highlight-post.png',
+    date: new Date(),
+    highlight: true
+  }
+]
+
+export const monthNames = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'June',
+  'July',
+  'Aug',
+  'Sept',
+  'Oct',
+  'Nov',
+  'Dec'
+]
---
+++ pages/posts/types.ts
@@ -0,0 +1,9 @@
+export interface PostProps {
+  id: string
+  title: string
+  description: string
+  text: string
+  image: any
+  date: Date
+  highlight?: boolean
+}
