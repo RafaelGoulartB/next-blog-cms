5b6b7f306ffc40ca85b4ae07033b5acc624ae890
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-23 09:15:46 -0300

    feat: add TextProps to date component

--- components/post-date/index.tsx
+++ components/post-date/index.tsx
@@ -1,13 +1,13 @@
-import { Text } from '@chakra-ui/react'
+import { Text, TextProps } from '@chakra-ui/react'
 import { NextPage } from 'next'
 import { monthNames } from '../section/postListSection/constants'
 
 interface Props {
   date: any
 }
-const PostDate: NextPage<Props> = ({ date }) => {
+const PostDate: NextPage<Props & TextProps> = ({ date, ...props }) => {
   return (
-    <Text color="gray.300">{`${
+    <Text color="gray.300" {...props}>{`${
       monthNames[new Date(date).getMonth()]
     } ${new Date(date).getDate()}th ${new Date(date).getFullYear()}`}</Text>
   )
