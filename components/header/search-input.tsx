4a2a622e399c78e5fe9ff4795f74f5f49c165db1
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-26 09:07:43 -0300

    feat: remove unnecessary imports

--- components/header/search-input.tsx
+++ components/header/search-input.tsx
@@ -4,8 +4,7 @@ import {
   FormControl,
   Input,
   InputGroup,
-  InputLeftElement,
-  InputRightElement
+  InputLeftElement
 } from '@chakra-ui/react'
 import { useRouter } from 'next/dist/client/router'
 
