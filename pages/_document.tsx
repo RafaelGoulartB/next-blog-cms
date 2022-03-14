5c2c9ae4cd7f2d0974bb7b2a317d29cb131213e2
RafaelGoulartB <rafagoulartb@gmail.com>
2022-03-14 16:08:52 -0300

    test nuvidio

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -45,17 +45,28 @@ class MyDocument extends Document {
           <NextScript />
         </body>
 
-        <script
+        {/* <div
           dangerouslySetInnerHTML={{
             __html: `
-            window.NuVidioId = "rafael.nuvidio.test";
-            window.NuVidioConfigs = {
-              fabButton: true,
-            }
-          `,
+            <script>
+              window.NuVidioId = "rafael.nuvidio.test";
+              window.NuVidioConfigs = {
+                fabButton: true,
+              }
+            </script>
+
+            <script type="text/javascript" src="https://hmlwidget.nuvidio.com/init.js" async></script>
+          `
+          }}
+        /> */}
+
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `
+            <script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="dc8ae365-5602-4692-8174-333d69a515c9";(function(){d = document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+          `
           }}
         />
-        <script type="text/javascript" src="https://hmlwidget.nuvidio.com/init.js" async></script>
       </Html>
     )
   }
